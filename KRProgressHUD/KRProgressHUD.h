179d9da48fdcd5138db05da44c34090257fe986e
krimpedance <krimpedance@gmail.com>
2016-06-04 21:16:02 +0900

    Add Carthage

---
+++ KRProgressHUD/KRProgressHUD.h
@@ -0,0 +1,19 @@
+//
+//  KRProgressHUD.h
+//  KRProgressHUD
+//
+//  Created by Ryunosuke Kirikihira on 2016/06/04.
+//  Copyright © 2016年 Krimpedance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KRProgressHUD.
+FOUNDATION_EXPORT double KRProgressHUDVersionNumber;
+
+//! Project version string for KRProgressHUD.
+FOUNDATION_EXPORT const unsigned char KRProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KRProgressHUD/PublicHeader.h>
+
+
