c3cb1502d1770bb1cefb208ec0ed0eb016b863fb
krimpedance <krimpedance@gmail.com>
2016-07-15 12:44:52 +0900

    Fixed an infinite recursion prefersStatusBarHidden()

--- KRProgressHUD/Classes/KRProgressHUDViewController.swift
+++ KRProgressHUD/Classes/KRProgressHUDViewController.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 class KRProgressHUDViewController: UIViewController {
+    var statusBarHidden = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(white: 0, alpha: 0.4)
     }
 
     override func prefersStatusBarHidden() -> Bool {
-        guard let vc = UIApplication.topViewController() else { return false }
-        return vc.prefersStatusBarHidden()
+        guard let vc = UIApplication.topViewController() else { return statusBarHidden }
+        if !vc.isKindOfClass(KRProgressHUDViewController) {
+            statusBarHidden = vc.prefersStatusBarHidden()
+        }
+        return statusBarHidden
     }
 }
