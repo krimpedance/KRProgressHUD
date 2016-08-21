42fa002f26bc1f091ee7c502981f849e377dbf92
krimpedance <krimpedance@gmail.com>
2016-08-21 15:09:13 +0900

    Fixed bug which HUD doesn't respect status bar style

--- KRProgressHUD/Classes/KRProgressHUDViewController.swift
+++ KRProgressHUD/Classes/KRProgressHUDViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class KRProgressHUDViewController: UIViewController {
+    var statusBarStyle = UIStatusBarStyle.Default
     var statusBarHidden = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(white: 0, alpha: 0.4)
+    }
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        guard let vc = UIApplication.topViewController() else { return statusBarStyle }
+        if !vc.isKindOfClass(KRProgressHUDViewController) {
+            statusBarStyle = vc.preferredStatusBarStyle()
+        }
+        return statusBarStyle
     }
 
     override func prefersStatusBarHidden() -> Bool {
