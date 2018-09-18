2ed9b12b087cbeed71444ffb234b2bdfecf120fd
koofrank <koofranker@gmail.com>
2018-09-18 14:11:58 +0800

    support swift4.2

--- KRProgressHUD/Classes/Extensions.swift
+++ KRProgressHUD/Classes/Extensions.swift
@@ -28,7 +28,7 @@ extension UIApplication {
 // MARK: - NSLayoutConstraint extension ------------
 
 extension NSLayoutConstraint {
-    convenience init(item view1: Any, attribute attr1: NSLayoutAttribute, relatedBy relation: NSLayoutRelation = .equal, toItem view2: Any? = nil, attribute attr2: NSLayoutAttribute? = nil, constant: CGFloat = 0) {
+    convenience init(item view1: Any, attribute attr1: NSLayoutConstraint.Attribute, relatedBy relation: NSLayoutConstraint.Relation = .equal, toItem view2: Any? = nil, attribute attr2: NSLayoutConstraint.Attribute? = nil, constant: CGFloat = 0) {
         self.init(item: view1, attribute: attr1, relatedBy: relation, toItem: view2, attribute: attr2 ?? attr1, multiplier: 1.0, constant: constant)
     }
 }
